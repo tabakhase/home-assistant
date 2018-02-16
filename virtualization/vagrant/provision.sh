102d49cf66944e30e4351b08a5266b1bdabf641a
Tabakhase <github@tabakhase.com>
2018-02-16 09:06:53 +0100

    vagrant dev - allow "../provision.sh ui-*" calls usefull when working inside the ui-repo

--- virtualization/vagrant/provision.sh
+++ virtualization/vagrant/provision.sh
@@ -183,6 +183,16 @@ run_ui_build() {
 }
 
 main() {
+    ## normalise location to allow "../provision.sh ui-*" calls when working inside the ui-repo
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+      SOURCE="$(readlink "$SOURCE")"
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    cd "$DIR"
+
     # If a parameter is provided, we assume it's the user interacting
     # with the provider script...
     case $1 in
